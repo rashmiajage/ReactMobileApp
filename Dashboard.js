5db6a3972831f01dbc77e00fea278bfc93df30b0
rashmiajage <70268666+rashmiajage@users.noreply.github.com>
2025-07-24 15:55:41 +0530

    Create Dashboard.js

---
+++ Dashboard.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Dashboard() {
+  return (
+    <div className="mobile-frame">
+      <div className="dashboard-box">
+        <h2>Welcome to Dashboard</h2>
+        <p>This is your mobile banking dashboard.</p>
+      </div>
+    </div>
+  );
+}
+
+export default Dashboard;
